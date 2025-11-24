cf50d74a124822a7d3cb5d2a02e0a6320653a68c
GMK <mk4400320@gmail.com>
2025-11-24 21:53:15 +0530

    cleaned some code, and extra unused symbols

--- src/main/java/in/krish/service/UserService.java
+++ src/main/java/in/krish/service/UserService.java
@@ -1,20 +1,12 @@
 package in.krish.service;
 
-import java.util.List;
-import in.krish.binding.CreatePost;
-import in.krish.binding.LoginForm;
-import in.krish.binding.RegisterForm;
-import in.krish.entity.Post;
+import in.krish.binding.UserDTO;
+import org.springframework.data.domain.Page;
+
 
 public interface UserService {
 
-	public String login(LoginForm form);
 
-	public boolean registrationForm(RegisterForm form);
-
-	public boolean createPost(CreatePost form);
-
-	public List<Post> getDashboardData(Integer userId, Post post);
-
+	public Page<UserDTO> searchUsers(String query, int page, int size);
 
 }
