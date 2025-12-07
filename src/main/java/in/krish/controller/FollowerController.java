bb1c47a2fea67f29d8c294a9d0975d8d7dda45e3
GMK <mk4400320@gmail.com>
2025-12-07 12:58:46 +0530

    Added user entity class and defined get user by id API

--- src/main/java/in/krish/controller/FollowerController.java
+++ src/main/java/in/krish/controller/FollowerController.java
@@ -2,9 +2,11 @@ package in.krish.controller;
 
 import in.krish.service.FollowerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/follow")
@@ -13,18 +15,25 @@ public class FollowerController {
     @Autowired
     private FollowerService followerService;
 
-    // Follow a user
-    @PostMapping("/{followingId}")
-    public String followUser(@PathVariable Long followingId, Principal principal) {
-        String email = principal.getName();
-        return followerService.followUser(followingId, email);
+    @PostMapping("/follow/{targetId}")
+    public ResponseEntity<?> followUser(@PathVariable Long targetId,
+                                        @RequestHeader("X-USER-ID") Long userId) {
+        followerService.follow(userId, targetId);
+        return ResponseEntity.ok(Map.of("message", "Followed"));
     }
 
-    // Unfollow a user
-    @DeleteMapping("/{followingId}")
-    public String unfollowUser(@PathVariable Long followingId, Principal principal) {
-        String email = principal.getName();
-        return followerService.unfollowUser(followingId, email);
+    @PostMapping("/unfollow/{targetId}")
+    public ResponseEntity<?> unfollowUser(@PathVariable Long targetId,
+                                          @RequestHeader("X-USER-ID") Long userId) {
+        followerService.unfollow(userId, targetId);
+        return ResponseEntity.ok(Map.of("message", "Unfollowed"));
+    }
+
+    @GetMapping("/status/{targetId}")
+    public ResponseEntity<?> checkStatus(@PathVariable Long targetId,
+                                         @RequestHeader("X-USER-ID") Long userId) {
+        boolean status = followerService.checkFollowStatus(userId, targetId);
+        return ResponseEntity.ok(Map.of("isFollowing", status));
     }
 }
 
